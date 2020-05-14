9201421fb68f866b0902fb3ea7ec173420b74bb7
Apurva Srivastava <26972786+apurva5297@users.noreply.github.com>
2020-05-15 02:27:18 +0530

    Add files via upload

---
+++ JSON server/public/precache-manifest.c9048a98f42e239e1add7c807955413d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fef7ae7916c5af09445519ba039f761b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc11917e7ad9282540a1",
+    "url": "/static/css/2.4173b2d3.chunk.css"
+  },
+  {
+    "revision": "3faced7f388230d1e879",
+    "url": "/static/css/main.d6bcb662.chunk.css"
+  },
+  {
+    "revision": "cc11917e7ad9282540a1",
+    "url": "/static/js/2.1c502e23.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.1c502e23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3faced7f388230d1e879",
+    "url": "/static/js/main.f33d1607.chunk.js"
+  },
+  {
+    "revision": "84926f692196015996db",
+    "url": "/static/js/runtime-main.c71f3b90.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
